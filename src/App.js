f8213a7924032960c42d9b6ca455ddcdb23e6f44
Patryk Jeziorowski <patryk.jeziorowski@gmx.com>
2021-12-03 11:00:58 +0100

    fea: env msg

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
+const msg = process.env.REACT_APP_MSG
+
 function App() {
+  console.log(msg)
   return (
     <div className="App">
       <header className="App-header">
@@ -15,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {msg}
         </a>
       </header>
     </div>
